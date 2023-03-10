8e53cc2db02c8ba8a68370a5a75cede909abfce7
juanma <juanmalujantmp@Gmail.com>
2023-03-09 21:15:29 -0300

    First Commit

---
+++ src/components/CartWidget/CartWidget.js
@@ -0,0 +1,11 @@
+import logo from './Carrito.png';
+const CartWidget = () => {
+    return (
+        <div className='Cart'>     
+            <img src={logo} alt="Logo" id='carrito'/>
+            {0}
+        </div>
+    )
+}
+
+export default CartWidget
